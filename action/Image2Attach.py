48dc67441ac76f9b7c3b6d44899a03278ff5d724
alswl <alswlx@gmail.com>
2011-12-18 12:31:18 +0800

    add fetch image and save attachments function

--- action/Image2Attach.py
+++ action/Image2Attach.py
@@ -29,10 +29,19 @@ class Image2Attach:
         self.pagename = pagename
         self.request = request
         self.page = Page(request, pagename)
+        self.image_urls = []
+        self.images = {}
+        self.process_success = 0
+        self.process_fail = 0
 
     def execute(self):
-        logging.info('****************************')
         self.getImageUrls()
+        self.fetchImages()
+        #logging.info(self.images)
+        logging.info('****************************')
+        logging.info(self.images.keys())
+        self.addAttachment()
+        logging.info(self.image_urls)
 
     def getImageUrls(self):
         """match all internet image url from raw"""
@@ -41,19 +50,33 @@ class Image2Attach:
             line = line.strip()
             match = WikiParser.scan_re.match(line)
             if match != None:
-               transclude = match.groupdict().get('transclude', '')
-               if transclude != None and transclude.find('attachment') < 0:
-                   transcludes.append(transclude)
-        self.images = [self.image_url_re.findall(x) for x in transcludes]
-        logging.info(self.images)
+                transclude = match.groupdict().get('transclude', '')
+                #logging.info('transclude')
+                #logging.info(transclude )
+                if transclude != None and transclude.find('attachment') < 0:
+                    # not attachment image ye
+                    transcludes.append(transclude)
+        # remove the {{ and }}
+        self.image_urls = [self.image_url_re.findall(x)[0] for x in transcludes]
 
     def fetchImages(self):
         """save image to local"""
-        pass
+        for url in self.image_urls:
+            try:
+                handler = urllib2.urlopen(url)
+                self.images[url] = handler.read()
+            except:
+                self.process_fail += 1
 
     def addAttachment(self):
         """add image to attachment"""
-        pass
+        for name, image in self.images.items():
+            AttachFile.add_attachment(
+                self.request,
+                self.pagename,
+                name,
+                image,
+                )
 
     def replaceImages(self):
         """edit the raw, replace image url with attachment url"""
